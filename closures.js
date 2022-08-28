4edde87c5ee89bfe3c6de2f4bf7648168d4413a3
hanafiabdilah <handilah81@gmail.com>
2022-08-28 09:40:03 +0700

    tambah contoh kedua

--- closures.js
+++ closures.js
@@ -1,6 +1,8 @@
 // Closure
-// Sebuah function yang ada didalam function lain, dan inner function tersebut mengakses variable yang ada di luar scopenya / outer functionnya.
+// 1. Sebuah function yang ada didalam function lain, dan inner function tersebut mengakses variable yang ada di luar scopenya / outer functionnya.
+// 2. Sebuah function yang mengembalikan / return function lain.
 
+// 1
 const outerFunction = () => {
   const nama = 'Hanafi'
 
@@ -12,3 +14,12 @@ const outerFunction = () => {
 }
 
 outerFunction()
+
+// 2
+const sayHello = (time) => {
+  return (name) => {
+    console.log(`Hello ${name}, good ${time}`)
+  }
+}
+
+sayHello('Morning')('Hanafi')
